b60ffe59db19c4e5cdba49703636cf664349be17
Justin Stout <midwatch@jstout.us>
2021-11-05 17:46:23 -0700

    add gcloud deps

--- bin/vagrant_provision.sh
+++ bin/vagrant_provision.sh
@@ -3,8 +3,9 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Set Time Zone"
 timedatectl set-timezone America/Los_Angeles
 
-echo "Set Env Vars"
-ln -sf /home/vagrant/config/pypi-token.sh /etc/profile.d/pypi-token.sh
+echo "Adding third party PPAs"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
 echo "Add Ubuntu Dependencies"
 apt-get update
