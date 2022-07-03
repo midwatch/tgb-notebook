77f94eeda3de24cf2d6f300bfc292d1994745125
Justin Stout <midwatch@jstout.us>
2022-07-03 14:52:30 -0700

    done

--- tasks.py
+++ tasks.py
@@ -4,6 +4,8 @@ from invoke import Collection
 from invoke import task
 from invoke.exceptions import Failure
 
+from mw_dry_invoke import git
+
 GITHUB_USERNAME = "midwatch"
 GITHUB_SLUG = "tgb-notebook"
 CC_VERSION = "0.0.0"
@@ -26,47 +28,6 @@ def clean_build(ctx):
     ctx.run("rm -fr build/")
 
 
-@task
-def scm_init(ctx):
-    """Init scm repo (if required).
-
-    Raises:
-        Failure: .gitignore does not exist
-
-    Returns:
-        None
-    """
-    if not Path('.gitignore').is_file():
-        raise Failure('.gitignore does not exist')
-
-    if not Path('.git').is_dir():
-        uri_remote = 'git@github.com:{}/{}.git'.format(GITHUB_USERNAME,
-                                                       GITHUB_SLUG
-                                                      )
-
-        ctx.run('git init')
-        ctx.run('git add .')
-        ctx.run('git commit -m "new package from midwatch/cc-py3-pkg ({})"'.format(CC_VERSION))
-        ctx.run('git branch -M main')
-        ctx.run('git remote add origin {}'.format(uri_remote))
-        ctx.run('git tag -a "v_0.0.0" -m "cookiecutter ref"')
-
-
-@task
-def scm_push(ctx):
-    """Push all branches and tags to origin."""
-
-    for branch in ('develop', 'main'):
-        ctx.run('git push origin {}'.format(branch))
-
-    ctx.run('git push --tags')
-
-
-@task
-def scm_status(ctx):
-    """Show status of remote branches."""
-    ctx.run('git for-each-ref --format="%(refname:short) %(upstream:track)" refs/heads')
-
 
 @task(pre=[clean_build])
 def clean(ctx):
@@ -88,14 +49,9 @@ def build(ctx):
 
 
 @task
-def init(ctx):
+def init_repo(ctx):
     """Initialize freshly cloned repo"""
-    scm_init(ctx)
-
-    ctx.run('git flow init -d')
-    ctx.run('git flow config set versiontagprefix v_')
-
-    scm_push(ctx)
+    git.init(ctx)
 
 
 @task(pre=[clean, build])
@@ -113,9 +69,6 @@ def release(ctx):
     ctx.run(cmd)
     ctx.run('gcloud app deploy')
 
-scm = Collection()
-scm.add_task(scm_push, name="push")
-scm.add_task(scm_status, name="status")
 
-ns = Collection(build, clean, init, release)
-ns.add_collection(scm, name="scm")
+ns = Collection(build, clean, init_repo, release)
+ns.add_collection(git.collection, name="scm")
